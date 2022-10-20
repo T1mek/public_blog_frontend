dbb4656c81c21ed4ebd65d1dcadb748175bb04a7
T1mek <misha1234567890m@gmail.com>
2022-10-20 18:05:45 +0300

    Добавление данных с backend и вывод на Frontend

---
+++ src/redux/slice/PostsSlice.js
@@ -0,0 +1,66 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import axios from "../../axois";
+
+
+export const fetchPosts = createAsyncThunk(
+    'posts/fetchPosts',
+    async () => {
+        const {data} = await axios.get('/posts')
+        return data
+    })
+export const fetchTags = createAsyncThunk(
+    'posts/fetchTags',
+    async () => {
+        const {data} = await axios.get('/tags')
+        return data
+    })
+
+
+const initialState = {
+    posts: {
+        items: [],
+        status: 'loading'
+    },
+    tags: {
+        items: [],
+        status: 'loading'
+    },
+}
+
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducer: {},
+    extraReducers: {
+
+        [fetchPosts.pending]: (state) => {
+            state.posts.status = 'loading'
+            state.posts.items = []
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.posts.items = action.payload
+            state.posts.status = 'loaded'
+        },
+        [fetchPosts.rejected]: (state) => {
+            state.posts.status = 'error'
+            state.posts.items = []
+        },
+        [fetchTags.pending]: (state) => {
+            state.tags.status = 'loading'
+            state.tags.items = []
+        },
+        [fetchTags.fulfilled]: (state, action) => {
+            state.tags.items = action.payload
+            state.tags.status = 'loaded'
+        },
+        [fetchTags.rejected]: (state) => {
+            state.tags.status = 'error'
+            state.tags.items = []
+        },
+    },
+
+
+})
+
+export const postsReducer = postsSlice.reducer
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {postsReducer} from "./slice/PostsSlice";
+
+ const store = configureStore({
+    reducer:{
+        posts:postsReducer,
+    }
+})
+
+
+export default store
+
